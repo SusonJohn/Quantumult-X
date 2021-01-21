5922b2f179061c5c25c57d5d970a5ae88b785fa5
xqy <10539670@qq.com>
2021-01-21 16:41:18 +0800

    crazyJoy领币

--- Script/jd_nianCollect.js
+++ Script/jd_nianCollect.js
@@ -152,12 +152,12 @@ function TotalBean() {
 function taskGetUrl(function_id, body) {
     return {
         url: `${JD_API}?body=${escape(JSON.stringify(body))}&appid=crazy_joy&functionId=${function_id}`,
-        data: {"functionId": ${function_id}},
         headers: {
             'Cookie': cookie,
             'Host': 'api.m.jd.com',
             'Accept': '*/*',
             'Connection': 'keep-alive',
+            'Referer': 'https://crazy-joy.jd.com',
             'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.2.2;14.2;%E4%BA%AC%E4%B8%9C/9.2.2 CFNetwork/1206 Darwin/20.1.0"),
             'Accept-Language': 'zh-cn',
             'Accept-Encoding': 'gzip, deflate, br',
